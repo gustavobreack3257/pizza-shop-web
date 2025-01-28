9e0aa30e3b5ac13028f8dea83f1e655650bcb0cd
Gustavo Costa Souza <drt65211@MAC432701-M.local>
2025-01-28 15:30:26 -0300

    feat:  Data Fishing in next.js

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,7 +11,7 @@ import ShirtProductFour from '../assets/camisetas/t-shirt4.png'
 import 'keen-slider/keen-slider.min.css'
 
 
-export default function Home() {
+export default function Home(props: { list: any; }) {
   const [sliderRef] = useKeenSlider({
     slides: {
       perView: 3,
@@ -23,6 +23,7 @@ export default function Home() {
   })
   return (
     <HomeContainer ref={sliderRef} className="keen-slider">
+      <pre>{JSON.stringify(props.list)}</pre>
       <Product className="keen-slider__slide">
         <Image src={ShirtProductOne} height={480} alt="Imagem do Produto" priority/>
 
@@ -61,4 +62,16 @@ export default function Home() {
 
     </HomeContainer>
   );
+}
+
+export const getServerSideProps = async () => {
+  await new Promise(resolve => setTimeout(resolve, 2000))
+
+  console.log('rodou')
+
+  return {
+    props: {
+      list: [1, 2, 3]
+    }
+  }
 }
