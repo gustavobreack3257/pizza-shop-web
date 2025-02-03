822cca3e6366bf264b7c33f551a0b0eaf78726a5
Gustavo Costa Souza <drt65211@MAC432701-M.local>
2025-02-03 10:11:16 -0300

    feat:  Integration with Stripe - structure product page

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,9 +1,23 @@
 import { useRouter } from "next/router";
+import { ImageContainer, ProductContainer, ProductDetails } from "../../styles/pages/product"
 
 export default function Product(){
     const {query} = useRouter()
 
     return(
-        <h1>Product {JSON.stringify(query)}</h1>
+        <ProductContainer>
+            <ImageContainer>
+
+            </ImageContainer>
+            <ProductDetails>
+                <h1>Camiseta Igniter</h1>
+                <span>R$ 54,90</span>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem praesentium voluptatum omnis deserunt at, dolores minima provident assumenda voluptatem culpa neque aspernatur nemo vitae! Cum cupiditate incidunt quae nisi nemo?</p>
+
+                <button>
+                    Comprar agora
+                </button>
+            </ProductDetails>
+        </ProductContainer>
     )
 }
