7404f730d8a72f57cb6aeb60fd8fe716d089e808
Gustavo Costa Souza <drt65211@MAC432701-M.local>
2025-02-06 13:07:53 -0300

    feat: Actualy

--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -1,16 +1,30 @@
-import Link from "next/link";
 import { ImageContainer, SuccessContainer } from "../styles/pages/success";
 
-export default function Success() {
+import Image from "next/image";
+import { GetServerSideProps } from "next";
+import Link from "next/link";
+import { stripe } from "../lib/stripe";
+import Stripe from "stripe";
+
+interface SuccessProps {
+  costumerName: string;
+  product: {
+    name: string
+    imageUrl: string
+  }
+}
+export default function Success({costumerName, product}: SuccessProps) {
   return (
     <SuccessContainer>
       <h1>Compra realizada</h1>
 
-      <ImageContainer></ImageContainer>
+      <ImageContainer>
+        <Image src={product.imageUrl} width={120} height={110} alt="Imagem do produto" />
+      </ImageContainer>
 
       <p>
-        Uhuul <strong>Diego Fernandes</strong>, sua{" "}
-        <strong>Camiseta Beyond the Limits</strong> já está a caminho da sua
+        Uhuul <strong>{costumerName}</strong>, sua{" "}
+        <strong>{product.name}</strong> já está a caminho da sua
         casa.
       </p>
 
@@ -19,4 +33,28 @@ export default function Success() {
       </Link>
     </SuccessContainer>
   );
+}
+
+export const getServerSideProps: GetServerSideProps = async ({ query }) => {
+  console.log('query', query.session_Id)
+  const sessionId = String(query.session_id);
+
+  console.log("ID",sessionId)
+
+  const session = await stripe.checkout.sessions.retrieve(sessionId, {
+    expand: ['line_items', 'line_items.data.price.product']
+  });
+
+  console.log("requisicao",session)
+  const costumerName = session.customer_details.name;
+  const product = session.line_items.data[0].price.product as Stripe.Product;
+  return {
+    props: {
+      costumerName,
+      product: {
+        name: product.name,
+        imageUrl: product.images[0]
+      }
+    }
+  }
 }
