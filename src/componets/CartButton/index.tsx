ab98ad695603b8d15ac89e6061e23bcea710a674
Gustavo Costa Souza <drt65211@MAC432701-M.local>
2025-02-18 09:06:23 -0300

    feat: Styled CartButton Component

--- src/componets/CartButton/index.tsx
+++ src/componets/CartButton/index.tsx
@@ -1,7 +1,16 @@
-export function CartButton(){
+import { ComponentProps } from 'react'
+import {CartButtonContainer} from './styles'
+import { Handbag } from '@phosphor-icons/react'
+import { Span } from 'next/dist/trace'
+
+type CartButtonProps = ComponentProps<typeof CartButtonContainer> & {
+    quantity?: number
+}
+export function CartButton({quantity = 0,...rest}: CartButtonProps){
     return(
-        <>
-            <p>Botão</p>
-        </>
+        <CartButtonContainer {...rest}>
+            {quantity > 0 && <span>{quantity}</span>}
+            <Handbag weight='bold'/>
+        </CartButtonContainer>
     )
 }
